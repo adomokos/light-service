07398e4fa8dc2747056378ee6bfc4e6578323023
Marc Ignacio <pi@sky.local>
2013-06-13 23:01:07 +0800

    Pass user instead of context to Organizer#with

--- spec/organizer_spec.rb
+++ spec/organizer_spec.rb
@@ -10,12 +10,13 @@ describe LightService::Organizer do
       with(user: user).reduce [AnAction]
     end
 
-    def self.some_method_with(context)
+    def self.some_method_with(user)
+      context = LightService::Context.new(user: user)
       with(context).reduce [AnAction]
     end
   end
 
-  let(:context) { ::LightService::Context.new(user: user) }
+  let!(:context) { ::LightService::Context.new(user: user) }
   let(:user) { double(:user) }
 
   context "when #with is called with hash" do
@@ -33,12 +34,13 @@ describe LightService::Organizer do
 
   context "when #with is called with Context" do
     it "does not create a context" do
+      LightService::Context.stub(:new).with(user: user).and_return(context)
       LightService::Context.should_not_receive(:make)
 
       AnAction.should_receive(:execute) \
               .with(context) \
               .and_return context
-      AnOrganizer.some_method_with(context)
+      AnOrganizer.some_method_with(user)
     end
   end
 
