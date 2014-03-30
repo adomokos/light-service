1dfc32aa45ffa23cd2d64f8c7f41443ee870d179
Attila Domokos <adomokos@gmail.com>
2014-03-30 09:51:31 -0500

    The method `reduce` accepts variable number of arguments

--- spec/sample/tax/calculates_tax.rb
+++ spec/sample/tax/calculates_tax.rb
@@ -2,11 +2,10 @@ class CalculatesTax
   include LightService::Organizer
 
   def self.for_order(order)
-    with(order: order).reduce \
-      [
+    with(:order => order).reduce(
         LooksUpTaxPercentageAction,
         CalculatesOrderTaxAction,
         ProvidesFreeShippingAction
-      ]
+    )
   end
 end
