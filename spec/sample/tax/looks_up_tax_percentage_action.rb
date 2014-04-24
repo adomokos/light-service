454afb13522fe526210c8ccfd1269724b56cf431
Attila Domokos <adomokos@gmail.com>
2014-04-24 17:47:48 -0500

    Adjusting the example to `promises`

--- spec/sample/tax/looks_up_tax_percentage_action.rb
+++ spec/sample/tax/looks_up_tax_percentage_action.rb
@@ -1,18 +1,17 @@
 class LooksUpTaxPercentageAction
   include LightService::Action
   expects :order
+  promises :tax_percentage
 
   executed do |context|
-    order = context.fetch(:order)
-    tax_ranges = TaxRange.for_region(order.region)
+    tax_ranges = TaxRange.for_region(self.order.region)
+    self.tax_percentage = 0
 
     next context if object_is_nil?(tax_ranges, context, 'The tax ranges were not found')
 
-    tax_percentage = tax_ranges.for_total(order.total)
+    self.tax_percentage = tax_ranges.for_total(self.order.total)
 
-    next context if object_is_nil?(tax_percentage, context, 'The tax percentage was not found')
-
-    context[:tax_percentage] = tax_percentage
+    next context if object_is_nil?(self.tax_percentage, context, 'The tax percentage was not found')
   end
 
   def self.object_is_nil?(object, context, message)
