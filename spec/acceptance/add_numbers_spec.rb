268db6b1ca590ef05a70474d22aacda43b2bf35a
Attila Domokos <adomokos@gmail.com>
2014-04-24 17:51:30 -0500

    Setting value through promises setter

--- spec/acceptance/add_numbers_spec.rb
+++ spec/acceptance/add_numbers_spec.rb
@@ -15,36 +15,39 @@ end
 class AddsOneAction
   include LightService::Action
   expects :number
+  promises :number
 
   executed do |context|
     number = self.number
     number += 1
 
-    context[:number] = number
+    self.number = number
   end
 end
 
 class AddsTwoAction
   include LightService::Action
   expects :number
+  promises :number
 
   executed do |context|
     number = self.number
     number += 2
 
-    context[:number] = number
+    self.number = number
   end
 end
 
 class AddsThreeAction
   include LightService::Action
   expects :number
+  promises :number
 
   executed do |context|
     number = self.number
     number += 3
 
-    context[:number] = number
+    self.number = number
   end
 end
 
