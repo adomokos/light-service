2ef80bf064cc5bf23658336dc103281e9e629b24
Attila Domokos <adomokos@gmail.com>
2014-04-24 22:47:04 -0500

    Putting the ContextKeyVerifier into the LightService module

--- lib/light-service/context_key_verifier.rb
+++ lib/light-service/context_key_verifier.rb
@@ -1,35 +1,37 @@
-class ExpectedKeysNotInContextError < StandardError; end
-class PromisedKeysNotInContextError < StandardError; end
+module LightService
+  class ExpectedKeysNotInContextError < StandardError; end
+  class PromisedKeysNotInContextError < StandardError; end
 
-class ContextKeyVerifier
-  class << self
-    def verify_expected_keys_are_in_context(context, expected_keys)
-      verify_keys_are_in_context(context, expected_keys) do |not_found_keys|
-        fail ExpectedKeysNotInContextError, "expected #{format_keys(not_found_keys)} to be in the context"
-      end
-    end
-
-    def verify_promised_keys_are_in_context(context, promised_keys)
-      verify_keys_are_in_context(context, promised_keys) do |not_found_keys|
-        fail PromisedKeysNotInContextError, "promised #{format_keys(not_found_keys)} to be in the context"
-      end
-    end
-
-    private
-
-    def verify_keys_are_in_context(context, keys)
-      keys ||= context.keys
-
-      not_found_keys = keys - context.keys
-      unless not_found_keys.empty?
-        yield not_found_keys
+  class ContextKeyVerifier
+    class << self
+      def verify_expected_keys_are_in_context(context, expected_keys)
+        verify_keys_are_in_context(context, expected_keys) do |not_found_keys|
+          fail ExpectedKeysNotInContextError, "expected #{format_keys(not_found_keys)} to be in the context"
+        end
       end
 
-      context
-    end
+      def verify_promised_keys_are_in_context(context, promised_keys)
+        verify_keys_are_in_context(context, promised_keys) do |not_found_keys|
+          fail PromisedKeysNotInContextError, "promised #{format_keys(not_found_keys)} to be in the context"
+        end
+      end
 
-    def format_keys(keys)
-      keys.map{|k| ":#{k}"}.join(', ')
+      private
+
+      def verify_keys_are_in_context(context, keys)
+        keys ||= context.keys
+
+        not_found_keys = keys - context.keys
+        unless not_found_keys.empty?
+          yield not_found_keys
+        end
+
+        context
+      end
+
+      def format_keys(keys)
+        keys.map{|k| ":#{k}"}.join(', ')
+      end
     end
   end
 end
