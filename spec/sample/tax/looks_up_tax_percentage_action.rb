1093de8ec33f456079499b40d2693c6601c81245
Attila Domokos <adomokos@gmail.com>
2014-05-21 22:26:52 -0500

    Moving the `promises` and `expects` accessors to Context
    The concept of `self.order` does not make much sense. Where did that 
    variable or attribute reader come from? It makes a ton more sense to
    ask the Context itself to provide the accessors for the promised and
    expected keys.

--- spec/sample/tax/looks_up_tax_percentage_action.rb
+++ spec/sample/tax/looks_up_tax_percentage_action.rb
@@ -4,14 +4,14 @@ class LooksUpTaxPercentageAction
   promises :tax_percentage
 
   executed do |context|
-    tax_ranges = TaxRange.for_region(self.order.region)
-    self.tax_percentage = 0
+    tax_ranges = TaxRange.for_region(context.order.region)
+    context.tax_percentage = 0
 
     next context if object_is_nil?(tax_ranges, context, 'The tax ranges were not found')
 
-    self.tax_percentage = tax_ranges.for_total(self.order.total)
+    context.tax_percentage = tax_ranges.for_total(context.order.total)
 
-    next context if object_is_nil?(self.tax_percentage, context, 'The tax percentage was not found')
+    next context if object_is_nil?(context.tax_percentage, context, 'The tax percentage was not found')
   end
 
   def self.object_is_nil?(object, context, message)
