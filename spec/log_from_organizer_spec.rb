5ffdd8a4ac1ecc16925cf9340ed6c6695eabd67b
Attila Domokos <adomokos@gmail.com>
2014-08-03 10:45:57 -0500

    Adding all the specs for logging

--- spec/log_from_organizer_spec.rb
+++ spec/log_from_organizer_spec.rb
@@ -39,4 +39,29 @@ describe "Logs from organizer" do
     expect(log_message).to include(organizer_log_message)
   end
 
+  it "lists the keys in context before the actions are executed" do
+    organizer_log_message = "[LightService] -     keys in context: :tea, :milk, :coffee"
+    expect(log_message).to include(organizer_log_message)
+  end
+
+  it "lists the expects actions are expecting" do
+    organizer_log_message = "[LightService] -   expects: :tea, :milk"
+    expect(log_message).to include(organizer_log_message)
+    organizer_log_message = "[LightService] -   expects: :coffee, :milk"
+    expect(log_message).to include(organizer_log_message)
+  end
+
+  it "lists the promises actions are promising" do
+    organizer_log_message = "[LightService] -   promises: :milk_tea"
+    expect(log_message).to include(organizer_log_message)
+    organizer_log_message = "[LightService] -   promises: :latte"
+    expect(log_message).to include(organizer_log_message)
+  end
+
+  it "lists the keys in contect after the actions are executed" do
+    organizer_log_message = "[LightService] -     keys in context: :tea, :milk, :coffee, :milk_tea, :latte"
+    expect(log_message).to include(organizer_log_message)
+  end
+
+
 end
