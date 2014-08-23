52531116c70f7de0f04d106643bb57048f36c32d
Attila Domokos <adomokos@gmail.com>
2014-08-23 17:37:34 -0500

    Moving the KeyVerifier under the context dir

--- lib/light-service.rb
+++ lib/light-service.rb
@@ -4,7 +4,7 @@ require "light-service/version"
 
 require 'light-service/configuration'
 require 'light-service/context'
-require 'light-service/context_key_verifier'
+require 'light-service/context/key_verifier'
 require 'light-service/organizer/with_reducer'
 require 'light-service/organizer/with_reducer_log_decorator'
 require 'light-service/organizer/with_reducer_factory'
