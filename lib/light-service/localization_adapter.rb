fccf9686b71ce0bed778d027905a9cbb71ab24c2
Attila Domokos <adomokos@gmail.com>
2014-10-16 15:31:23 -0500

    DRYing up the LocalizationAdapter logic

--- lib/light-service/localization_adapter.rb
+++ lib/light-service/localization_adapter.rb
@@ -1,24 +1,29 @@
 module LightService
   class LocalizationAdapter
     def failure(message_or_key, action_class, i18n_options={})
-      if message_or_key.is_a?(Symbol)
-        i18n_options.merge!(type: :failure)
-        translate(message_or_key, action_class, i18n_options)
-      else
-        message_or_key
-      end
+      find_translated_message(message_or_key,
+                              action_class,
+                              i18n_options,
+                              {:type => :failure})
     end
 
     def success(message_or_key, action_class, i18n_options={})
+      find_translated_message(message_or_key,
+                              action_class,
+                              i18n_options,
+                              {:type => :success})
+    end
+
+    private
+
+    def find_translated_message(message_or_key, action_class, i18n_options, type)
       if message_or_key.is_a?(Symbol)
-        i18n_options.merge!(type: :success)
+        i18n_options.merge!(type)
         translate(message_or_key, action_class, i18n_options)
       else
         message_or_key
       end
     end
-
-    private
 
     def translate(key, action_class, options={})
       type = options.delete(:type)
