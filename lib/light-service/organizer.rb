61556552fc57ca6e62c8ee52edd17fedfad5146a
JP Moral <jp@jpmoral.com>
2015-07-04 08:56:45 +0800

    Remove unused methods

--- lib/light-service/organizer.rb
+++ lib/light-service/organizer.rb
@@ -18,15 +18,5 @@ module LightService
         WithReducerFactory.make(self).with.reduce(actions)
       end
     end
-
-    # Provide hooks for extending the class with these methods
-    def with(data = {})
-      WithReducerFactory.make(self).with(data)
-    end
-
-    def reduce(*actions)
-      WithReducerFactory.make(self).with.reduce(actions)
-    end
-
   end
 end
