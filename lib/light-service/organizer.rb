8b06d5643243d17cea65a72bb37436297d1983d2
JP Moral <jp@jpmoral.com>
2015-07-04 17:52:15 +0800

    Add `aliases` macro to Organizer
    aliases is passed to WithReducer to make key aliases available to all
    actions
    
    See spec/organizer_key_aliases_spec.rb

--- lib/light-service/organizer.rb
+++ lib/light-service/organizer.rb
@@ -2,20 +2,28 @@ module LightService
   module Organizer
     def self.extended(base_class)
       base_class.extend ClassMethods
+      base_class.extend Macros
     end
     def self.included(base_class)
       ActiveSupport::Deprecation.warn "including Lightervice::Organizer is deprecated. Please use `extend LightService::Organizer` instead"
       base_class.extend ClassMethods
+      base_class.extend Macros
     end
 
     # In case this module is included
     module ClassMethods
       def with(data)
-        WithReducerFactory.make(self).with(data)
+        WithReducerFactory.make(self).with(data, @aliases)
       end
 
       def reduce(*actions)
         with({}).reduce(actions)
+      end
+    end
+
+    module Macros
+      def aliases(key_hash)
+        @aliases = key_hash
       end
     end
   end
---
+++ spec/organizer_key_aliases_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'test_doubles'
+
+describe "organizer aliases macro" do
+  let(:organizer_with_alias) do
+    Class.new do
+      extend LightService::Organizer
+
+      aliases promised_key: :expected_key
+
+      def self.do_something(ctx={})
+        with(ctx).reduce([
+          TestDoubles::PromisesPromisedKeyAction,
+          TestDoubles::ExpectsExpectedKeyAction,
+        ])
+      end
+    end
+  end
+
+  context "when aliases is invoked" do
+    it "makes aliases available to the actions" do
+      result = organizer_with_alias.do_something
+      expect(result[:promised_key]).to eq result[:expected_key]
+    end
+  end
+end
