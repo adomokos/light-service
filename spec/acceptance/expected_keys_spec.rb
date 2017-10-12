9624add37cfd2eed7b88f932c813573cf9364f7a
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-10-11 21:58:03 -0500

    Expand the acceptance tests for verifying expected keys

--- spec/acceptance/expected_keys_spec.rb
+++ spec/acceptance/expected_keys_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-RSpec.describe "raises an error if expected keys are not used" do
-  class ExpectedKeysAction
+RSpec.describe "verifying expected keys are used" do
+  class OnlyExpectedKeysAction
     extend LightService::Action
     expects :one, :two
 
@@ -10,10 +10,53 @@ RSpec.describe "raises an error if expected keys are not used" do
     end
   end
 
-  it "raises an error" do
-    error_message = "Expected keys [:two] to be used during ExpectedKeysAction"
-    expect { ExpectedKeysAction.execute(:one => 1, :two => 2) }.to(
-      raise_error(LightService::ExpectedKeysNotUsedError, error_message)
-    )
+  class OnlyMaybeKeysAction
+    extend LightService::Action
+    expects :maybe => [:one, :two]
+
+    executed do |ctx|
+      ctx.one * 2
+    end
+  end
+
+  class SomeMaybeKeysAction
+    extend LightService::Action
+    expects :one, :maybe => :two
+
+    executed do |ctx|
+      ctx.one * 2
+    end
+  end
+
+  class UsesMaybeKeysAction
+    extend LightService::Action
+    expects :one, :maybe => :two
+    promises :product
+
+    executed do |ctx|
+      ctx.product = ctx.one * ctx.two
+    end
+  end
+
+  it "raises an error when expected keys aren't used" do
+    error_message = "Expected keys [:two] to be used during "\
+                    "OnlyExpectedKeysAction"
+    expect do
+      OnlyExpectedKeysAction.execute(:one => 1, :two => 2)
+    end.to(raise_error(LightService::ExpectedKeysNotUsedError, error_message))
+  end
+
+  it "doesn't raise an error if maybe keys aren't used" do
+    expect do
+      OnlyMaybeKeysAction.execute(:one => 1, :two => 2)
+    end.not_to(raise_error)
+    expect do
+      SomeMaybeKeysAction.execute(:one => 1, :two => 2)
+    end.not_to(raise_error)
+  end
+
+  it "accesses maybe keys like normal expects keys" do
+    result = UsesMaybeKeysAction.execute(:one => 8, :two => 11)
+    expect(result.product).to eq 88
   end
 end
