49d724333c2def5e39d0ffd9a8eeeb7d06251fd5
Attila Domokos <adomokos@gmail.com>
2018-01-01 13:15:01 -0500

    Externalize 'reduce_if' logic into its own class

--- lib/light-service/organizer.rb
+++ lib/light-service/organizer.rb
@@ -27,20 +27,7 @@ module LightService
       end
 
       def reduce_if(condition_block, steps)
-        lambda do |ctx|
-          return ctx if ctx.stop_processing?
-
-          ctx = scoped_reduction(ctx, steps) if condition_block.call(ctx)
-          ctx
-        end
-      end
-
-      def scoped_reduction(ctx, steps)
-        ctx.reset_skip_remaining! unless ctx.failure?
-        ctx = with(ctx).reduce([steps])
-        ctx.reset_skip_remaining! unless ctx.failure?
-
-        ctx
+        ReduceIf.run(self, condition_block, steps)
       end
 
       # We need to make sure existing users will
