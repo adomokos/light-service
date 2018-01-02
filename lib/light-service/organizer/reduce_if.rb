b9afe492caa8b6846908f59a656c359a18cc54ad
Attila Domokos <adomokos@gmail.com>
2018-01-01 20:11:26 -0500

    Extract the scoped_reduce in a module

--- lib/light-service/organizer/reduce_if.rb
+++ lib/light-service/organizer/reduce_if.rb
@@ -1,22 +1,16 @@
 module LightService
   module Organizer
     class ReduceIf
+      extend ScopedReducable
+
       def self.run(organizer, condition_block, steps)
         lambda do |ctx|
           return ctx if ctx.stop_processing?
 
-          ctx = scoped_reduction(organizer, ctx, steps) \
+          ctx = scoped_reduce(organizer, ctx, steps) \
                   if condition_block.call(ctx)
           ctx
         end
-      end
-
-      def self.scoped_reduction(organizer, ctx, steps)
-        ctx.reset_skip_remaining! unless ctx.failure?
-        ctx = organizer.with(ctx).reduce([steps])
-        ctx.reset_skip_remaining! unless ctx.failure?
-
-        ctx
       end
     end
   end
