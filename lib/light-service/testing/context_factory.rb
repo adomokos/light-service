a207d17296d9ff995e39ddeb12ed039159a11606
Attila Domokos <adomokos@gmail.com>
2018-03-30 16:13:20 -0500

    Revert "ContextFactory patch for Organizer methods" (#146)
    * Revert "Bump version for 0.10.1 and add release notes"
    
    This reverts commit 8a95a84d6c62045e1f0634d23d29175f99cec7d5.
    
    * Revert "ContextFactory patch for Organizer methods (#141)"
    
    This reverts commit 00738f61537fe846db3d4b82d9e46a2c6326e769.

--- lib/light-service/testing/context_factory.rb
+++ lib/light-service/testing/context_factory.rb
@@ -1,6 +1,17 @@
 module LightService
   module Testing
     class ContextFactory
+      class ContextFactoryOrganizer
+        extend LightService::Organizer
+        class << self
+          attr_accessor :actions
+        end
+
+        def self.call(ctx)
+          with(ctx).reduce(actions)
+        end
+      end
+
       attr_reader :organizer
 
       def self.make_from(organizer)
@@ -8,27 +19,24 @@ module LightService
       end
 
       def for(action)
-        @organizer.before_actions = [
-          lambda do |ctx|
-            if ctx.current_action == action
-              throw(:return_ctx_from_execution, ctx)
-            end
-          end
-        ]
-
+        ContextFactoryOrganizer.actions = find_up_to(action)
         self
       end
 
       def with(ctx)
-        escaped = catch(:return_ctx_from_execution) do
-          @organizer.call(ctx)
-        end
-
-        escaped
+        ContextFactoryOrganizer.call(ctx)
       end
 
       def initialize(organizer)
         @organizer = organizer
+      end
+
+      def find_up_to(action)
+        original_actions = organizer.actions
+
+        original_actions.take_while do |current_action|
+          current_action != action
+        end
       end
     end
   end
