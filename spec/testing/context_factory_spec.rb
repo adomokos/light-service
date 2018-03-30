490d8a277c3544b9b61f03a94d54a2597eeafbf5
Jeremy Hanna <jeremy-hanna@github.com>
2018-03-30 14:03:32 -0500

    Remove pending spec and move to open LS issue

--- spec/testing/context_factory_spec.rb
+++ spec/testing/context_factory_spec.rb
@@ -39,20 +39,4 @@ describe 'ContextFactory - used with AdditionOrganizer' do
       expect(ctx.number).to eq(4)
     end
   end
-
-  context 'when called with an invalid action' do
-    let(:invalid_action) { TestDoubles::MakesLatteAction }
-
-    xit 'raises an argument error' do
-      expect do
-        LightService::Testing::ContextFactory
-          .make_from(organizer)
-          .for(invalid_action)
-          .with(1)
-      end.to raise_error(
-        ArgumentError,
-        "Could not find action in #{organizer}"
-      )
-    end
-  end
 end
