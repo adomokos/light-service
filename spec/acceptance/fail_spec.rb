f2dc71ff1531af96eede32bc840295de85a0b2df
Ace Subido <ace.subido@gmail.com>
2019-05-11 00:07:26 +0800

    Add spec for expecting fail_and_return to also take in error_code options

--- spec/acceptance/fail_spec.rb
+++ spec/acceptance/fail_spec.rb
@@ -23,4 +23,28 @@ RSpec.describe "fail_and_return!" do
       expect(result.two).to be_nil
     end
   end
+
+  describe "accepts error_code option" do
+    class FailAndReturnWithErrorCodeAction
+      extend LightService::Action
+      promises :one, :two
+
+      executed do |ctx|
+        ctx.one = 1
+        # Have to set it in Context
+        ctx.two = nil
+
+        ctx.fail_and_return!('Something went wrong', :error_code => 401)
+        ctx.two = 2
+      end
+    end
+
+    it "returned context contains the error_code" do
+      result = FailAndReturnWithErrorCodeAction.execute
+
+      expect(result).to be_failure
+      expect(result.error_code).to eq 401
+      expect(result.two).to be_nil
+    end
+  end
 end
