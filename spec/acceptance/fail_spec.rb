d5e60c4fcec0e5ee4737d95479dbb12cc13a7358
Ace Subido <ace.subido@gmail.com>
2019-05-11 00:06:47 +0800

    Rename describe text for fail_and_return acceptance from fail! to fail_and_return

--- spec/acceptance/fail_spec.rb
+++ spec/acceptance/fail_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
-RSpec.describe "fail! returns immediately from executed block" do
-  class FailAction
-    extend LightService::Action
-    promises :one, :two
+RSpec.describe "fail_and_return!" do
+  describe "returns immediately from executed block" do
+    class FailAndReturnAction
+      extend LightService::Action
+      promises :one, :two
 
-    executed do |ctx|
-      ctx.one = 1
-      # Have to set it in Context
-      ctx.two = nil
+      executed do |ctx|
+        ctx.one = 1
+        # Have to set it in Context
+        ctx.two = nil
 
-      ctx.fail_and_return!('Something went wrong')
-      ctx.two = 2
+        ctx.fail_and_return!('Something went wrong')
+        ctx.two = 2
+      end
     end
-  end
 
-  it "returns immediately from executed block" do
-    result = FailAction.execute
+    it "returns immediately from executed block" do
+      result = FailAndReturnAction.execute
 
-    expect(result).to be_failure
-    expect(result.two).to be_nil
+      expect(result).to be_failure
+      expect(result.two).to be_nil
+    end
   end
 end
