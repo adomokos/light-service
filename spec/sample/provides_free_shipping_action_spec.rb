24eefab8c7f658cfc0a2eb5a41be30fd9bf9bfce
Attila Domokos <adomokos@gmail.com>
2019-12-06 08:22:42 -0600

    Fix the spec description, order <=200 should not have free shipping (#177)

--- spec/sample/provides_free_shipping_action_spec.rb
+++ spec/sample/provides_free_shipping_action_spec.rb
@@ -15,7 +15,7 @@ describe ProvidesFreeShippingAction do
   end
 
   context "when the order total with tax is <= 200" do
-    specify "order gets free shipping" do
+    specify "order does not get free shipping" do
       allow(order).to receive_messages(:total_with_tax => 200)
       expect(order).not_to receive(:provide_free_shipping!)
 
