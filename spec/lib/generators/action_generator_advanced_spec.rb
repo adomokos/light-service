27f66e71438000082193ea3818434d80ea8d577a
Gabriel Fortuna <gabriel@zero-one.io>
2020-06-28 14:21:24 +0200

    test: add specs for advanced action generation

---
+++ spec/lib/generators/action_generator_advanced_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+require_relative '../../../lib/generators/light_service/action_generator.rb'
+require_relative './full_generator_test_blobs'
+
+include FullGeneratorTestBlobs
+
+describe LightService::Generators::ActionGenerator, type: :generator do
+  destination File.expand_path("../tmp", __FILE__)
+
+  context "when generating an advanced action" do
+    before(:all) do
+      prepare_destination
+      run_generator
+    end
+
+    after(:all) do
+      FileUtils.rm_rf destination_root
+    end
+
+    arguments %w(my/fancy/action expects:foo,bar promises:baz,qux --no-roll-back --dir=services)
+
+    specify do
+      expect(destination_root).to have_structure {
+        directory "app/services/my/fancy" do
+          file "action.rb" do
+            contains advanced_action_blob
+          end
+        end
+
+        directory "spec/services/my/fancy" do
+          file "action_spec.rb" do
+            contains advanced_action_spec_blob
+          end
+        end
+      }
+    end
+  end
+end
