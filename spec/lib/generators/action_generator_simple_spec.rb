b3759e292a4a41c4632b7143043301feef2643b4
Gabriel Fortuna <gabriel@zero-one.io>
2020-06-28 12:09:54 +0200

    refactor: move simple action gen tests to their own file

--- spec/lib/generators/action_generator_simple_spec.rb
+++ spec/lib/generators/action_generator_simple_spec.rb
@@ -9,29 +9,25 @@ include FullGeneratorTestBlobs
 describe LightService::Generators::ActionGenerator, type: :generator do
   destination File.expand_path("../tmp", __FILE__)
 
-  before(:all) do
-    prepare_destination
-    run_generator
-  end
-
   context "when generating a simple action" do
+    before(:all) do
+      prepare_destination
+      run_generator
+    end
+
     arguments %w(my_action)
 
     specify do
       expect(destination_root).to have_structure {
-        directory "app" do
-          directory "actions" do
-            file "my_action.rb" do
-              contains simple_action_blob
-            end
+        directory "app/actions" do
+          file "my_action.rb" do
+            contains simple_action_blob
           end
         end
 
-        directory "spec" do
-          directory "actions" do
-            file "my_action_spec.rb" do
-              contains simple_action_spec_blob
-            end
+        directory "spec/actions" do
+          file "my_action_spec.rb" do
+            contains simple_action_spec_blob
           end
         end
       }
