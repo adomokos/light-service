839b6e5aa74d34b636cddd0b2fe843311752e6e3
Gabriel Fortuna <gabriel@zero-one.io>
2020-06-28 14:21:46 +0200

    test: add specs for advanced organizer generation

---
+++ spec/lib/generators/organizer_generator_advanced_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+require_relative '../../../lib/generators/light_service/organizer_generator.rb'
+require_relative './full_generator_test_blobs'
+
+include FullGeneratorTestBlobs
+
+describe LightService::Generators::OrganizerGenerator, type: :generator do
+  destination File.expand_path("../tmp", __FILE__)
+
+  context "when generating an advanced organizer" do
+    before(:all) do
+      prepare_destination
+      run_generator
+    end
+
+    after(:all) do
+      FileUtils.rm_rf destination_root
+    end
+
+    arguments %w(my/fancy/organizer --dir=processes)
+
+    specify do
+      expect(destination_root).to have_structure {
+        directory "app/processes/my/fancy" do
+          file "organizer.rb" do
+            contains advanced_organizer_blob
+          end
+        end
+
+        directory "spec/processes/my/fancy" do
+          file "organizer_spec.rb" do
+            contains advanced_organizer_spec_blob
+          end
+        end
+      }
+    end
+  end
+end
