add287aabf3948283978f40d788b75029260a57f
Gabriel Fortuna <gabriel@zero-one.io>
2021-07-02 16:32:08 +0200

    refactor: Context#define_accessor_methods_for_keys can accept single values or lists

--- lib/light-service/context.rb
+++ lib/light-service/context.rb
@@ -115,9 +115,9 @@ module LightService
     end
 
     def define_accessor_methods_for_keys(keys)
-      return if keys.nil?
+      return if keys.blank?
 
-      keys.each do |key|
+      Array(keys).each do |key|
         next if respond_to?(key.to_sym)
 
         define_singleton_method(key.to_s) { fetch(key) }
