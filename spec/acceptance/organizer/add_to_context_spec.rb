1b5ee03642c06714cfa1bec6228bec2926d8f59b
Gabriel Fortuna <gabriel@zero-one.io>
2021-07-02 16:33:44 +0200

    test: ensure that Organizer.add_to_context doesn't clobber Context reserved words

--- spec/acceptance/organizer/add_to_context_spec.rb
+++ spec/acceptance/organizer/add_to_context_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe LightService::Organizer do
         # with the value of 0, so it's available for
         # AddsOneAction
         add_to_context(:number => 0),
+        add_to_context(:message => "Just a message"),
+        add_to_context(:error_code => "D0H"),
         TestDoubles::AddsOneAction,
         add_to_context(:something => 'hello')
       ]
@@ -33,5 +35,17 @@ RSpec.describe LightService::Organizer do
 
     expect(result).to be_success
     expect(result.something).to eq('hello')
+  end
+
+  it "will not add items as accessors when they are reserved" do
+    result = TestAddToContext.call
+
+    expect(result).to be_success
+
+    expect(result.message).to be_blank
+    expect(result[:message]).to eq "Just a message"
+
+    expect(result.error_code).to be_blank
+    expect(result[:error_code]).to eq "D0H"
   end
 end
