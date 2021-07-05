1ce21ee6e6718017d3ac23ed2807526f11afd847
Gabriel Fortuna <gabriel@zero-one.io>
2021-07-05 12:03:23 +0200

    style: fix two rubocop issues

--- spec/acceptance/organizer/execute_with_add_to_context_spec.rb
+++ spec/acceptance/organizer/execute_with_add_to_context_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe LightService::Organizer do
     extend LightService::Organizer
 
     def self.call
-      with().reduce(steps)
+      with.reduce(steps)
     end
 
     def self.steps
@@ -19,7 +19,7 @@ RSpec.describe LightService::Organizer do
 
   context "when using context values created by add_to_context" do
     it "is expected to reference them as accessors" do
-      result = TestExecuteWithAddToContext.call()
+      result = TestExecuteWithAddToContext.call
 
       expect(result).to be_a_success
       expect(result.greeting).to eq "HELLO"
