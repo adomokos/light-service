c99de9d83065f98e5cbb367a62c31956a0e8e493
Gabriel Fortuna <gabriel@zero-one.io>
2021-08-29 17:11:59 +0200

    feat: build up context hash with default values when creating action ctx

--- lib/light-service/action.rb
+++ lib/light-service/action.rb
@@ -84,11 +84,33 @@ module LightService
       def create_action_context(context)
         return context if context.is_a? LightService::Context
 
+        usable_defaults(context).each do |ctx_key, default|
+          context[ctx_key] = extract_default(default, context)
+        end
+
         LightService::Context.make(context)
       end
 
       def all_keys
         expected_keys + promised_keys
+      end
+
+      def missing_expected_keys(context)
+        expected_keys - context.keys
+      end
+
+      def usable_defaults(context)
+        available_defaults.slice(
+          *(missing_expected_keys(context) & available_defaults.keys)
+        )
+      end
+
+      def extract_default(default, context)
+        is_callable = default.respond_to?(:call)
+
+        return default unless is_callable
+
+        default.call(context)
       end
 
       def call_before_action(context)
